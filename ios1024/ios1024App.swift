5fc1d892bfc5c23532b873c3cba3349f33cfb091
Hans Dulimarta <dulimarh@gvsu.edu>
2024-10-30 20:47:55 -0400

    Initial Commit

---
+++ ios1024/ios1024App.swift
@@ -0,0 +1,17 @@
+//
+//  ios1024App.swift
+//  ios1024
+//
+//  Created by Hans Dulimarta on 10/30/24.
+//
+
+import SwiftUI
+
+@main
+struct ios1024App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
